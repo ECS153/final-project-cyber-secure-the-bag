ab0a00f9b37dda5968de137b2313a87c568532f6
Cameron <cayuen@gmail.com>
2020-05-21 20:50:03 -0700

    Added template feature of emoji

---
+++ TrojanExtension/src/app/emoji/emoji.ts
@@ -0,0 +1,62 @@
+export const emojis = [
+    {
+      name: 'smile',
+      icon: '😀',
+      category: 'Faces',
+      row: '1'
+    },
+    {
+      name: 'wink',
+      icon: '😉',
+      category: 'Faces',
+      row: '1'
+    },
+    {
+      name: 'sad',
+      icon: '😔',
+      category: 'Faces',
+      row: '1'
+    },
+    {
+      name: 'horse',
+      icon: '🐴',
+      category: 'Animals',
+      row: '1'
+    },
+    {
+      name: 'pig',
+      icon: '🐖',
+      category: 'Animals',
+      row: '1'
+    },
+    {
+      name: 'goat',
+      icon: '🐐',
+      category: 'Animals',
+      row: '1'
+    },
+    {
+      name: 'Sheep',
+      icon: '🐑',
+      category: 'Animals',
+      row: '1'
+    },
+    {
+      name: 'Corn',
+      icon: '🌽',
+      category: 'Farm',
+      row: '1'
+    },
+    {
+      name: 'Tomato',
+      icon: '🍅',
+      category: 'Farm',
+      row: '1'
+    },
+    {
+      name: 'Eggplant',
+      icon: '🍆',
+      category: 'Farm',
+      row: '1'
+    }
+  ];
--- TrojanExtension/src/app/popup/popup.component.ts
+++ TrojanExtension/src/app/popup/popup.component.ts
@@ -1,28 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
-  selector: 'app-popup',
-  templateUrl: './popup.component.html',
-  styleUrls: ['./popup.component.css']
+    selector: 'app-popup',
+    templateUrl: './popup.component.html',
+    styleUrls: ['./popup.component.css']
 })
 export class PopupComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
-  copyEmoji(id) {
-    console.log('clicked');
+    // copyEmoji(id) {
+    //     console.log('clicked');
 
-    var button = document.getElementById(id) as HTMLButtonElement;
-    let unicode = button.textContent;
+    //     var button = document.getElementById(id) as HTMLButtonElement;
+    //     let unicode = button.textContent;
 
-    const el = document.createElement('textarea');
-    el.value = unicode;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-  }
+    //     const el = document.createElement('textarea');
+    //     el.value = unicode;
+    //     document.body.appendChild(el);
+    //     el.select();
+    //     document.execCommand('copy');
+    //     document.body.removeChild(el);
+    // }
+
+    openTab(cityName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(cityName).style.display = "block";
+    }
+
 }
