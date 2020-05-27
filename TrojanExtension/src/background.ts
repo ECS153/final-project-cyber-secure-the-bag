80c398b326b5d41ecc17e88903588b04a5b86469
Orli Feuchtwang <orli.feuchtwang@gmail.com>
2020-05-26 17:26:48 -0700

    cleaned up background and manifest

--- TrojanExtension/src/background.ts
+++ TrojanExtension/src/background.ts
@@ -1,13 +1,3 @@
-// chrome.runtime.onInstalled.addListener(() => {
-//     chrome.webNavigation.onCompleted.addListener(() => {
-//         chrome.tabs.query({ active: true, currentWindow: true },function(tabs) {
-//             if(tabs[0]) {chrome.tabs.sendMessage(tabs[0].id,{from: "background"});}
-            
-//         });
-//     }, { url: [{ urlMatches: 'messenger.com' } ] });
-
-// });
-
 chrome.runtime.onMessage.addListener(
     function(request, sender) {
         var urlLog = new Object();
