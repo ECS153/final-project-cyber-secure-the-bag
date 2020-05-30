3fc289e406abcd39fc68171f3a05a919bfb27b5e
trevca <trevorcarpenter4@gmail.com>
2020-05-29 20:25:52 -0700

    updates to the web app

--- TrojanApp/src/app/data.model.ts
+++ TrojanApp/src/app/data.model.ts
@@ -4,9 +4,14 @@ export class Site {
     fields: Map<string, string[]>;
     likely_fields: Map<string, string>;
     field_percentage: Map<string, number>;
+    show: Boolean;
+    average_percentage: number;
 }
 
 export class Data {
     user: string;
     sites: Site[];
+    show: Boolean;
+    average_percentage: number;
+    shown_sites: Site[];
 }
